18fc7bbccd90cab66be2b15473b4a247aac274c2
hirleata.andrei <hirleata.andrei@gmail.com>
2022-02-07 17:39:35 +0000

    fixed IE vulnerability

--- myApp.js
+++ myApp.js
@@ -11,6 +11,7 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
