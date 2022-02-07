9a52d6b91b55a90fd8e07af11f3a162e90530c05
hirleata.andrei <hirleata.andrei@gmail.com>
2022-02-07 20:29:59 +0000

    set up content security policy fix

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds , force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({directives: { defaultSrc:["'self'",'trusted-cdn.com']}  }))
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ))
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
