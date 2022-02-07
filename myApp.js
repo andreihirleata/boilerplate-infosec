72ffa572835dc13cdeda1f677b2f7bf2f829ec1b
hirleata.andrei <hirleata.andrei@gmail.com>
2022-02-07 17:19:19 +0000

    'X-Powered-By: Express' hidden

--- myApp.js
+++ myApp.js
@@ -2,55 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-helmet.hidePoweredBy();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+app.use(helmet.hidePoweredBy());
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
